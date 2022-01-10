189c287aa338ee62359a099fae9ec2537659b511
Damien Milon <37899667+dmilon@users.noreply.github.com>
2022-01-10 15:13:13 +0100

    adapt to ruby 3

--- json_demo.rb
+++ json_demo.rb
@@ -1,4 +1,4 @@
-require 'json'
+require "json"
 
-# TODO - let's read/write data from beers.json
-filepath    = 'data/beers.json'
+# TODO - let's read/write data from beatles.json
+filepath = "data/beatles.json"
