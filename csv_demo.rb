7bc98fd8a79cff4ac403352bb53447c154adefb1
mahongri <mahongri1993@gmail.com>
2025-02-28 23:02:37 -0500

    recap

--- csv_demo.rb
+++ csv_demo.rb
@@ -2,3 +2,23 @@ require "csv"
 
 # TODO - let's read/write data from beatles.csv
 filepath = "data/beatles.csv"
+
+# 1. Read the CSV file
+CSV.foreach(filepath, headers: :first_row) do |row|
+  # p row
+  # p "#{row[0]} #{row[1]} plays the #{row[2]}"
+  puts "#{row['First Name']} #{row['Last Name']} played #{row['Instrument']}"
+end
+
+# 2. Write the CSV file
+
+#r - read only
+#wb - write binary
+
+CSV.open(filepath, 'wb') do |csv|
+  csv << ["First Name", "Last Name", "Instrument"]
+  csv << ["John", "Lennon", "Guitar"]
+  csv << ["Paul", "McCartney", "Bass"]
+  csv << ["George", "Harrison", "Guitar"]
+  # csv << ["Ringo", "Starr", "Drums"]
+end
--- json_demo.rb
+++ json_demo.rb
@@ -2,3 +2,27 @@ require "json"
 
 # TODO - let's read/write data from beatles.json
 filepath = "data/beatles.json"
+
+beatles_serialized = File.read(filepath)
+p beatles_serialized
+beatles = JSON.parse(beatles_serialized)
+# p beatles["title"]
+
+beatles = { beatles: [
+  {
+    first_name: "John",
+    last_name: "Lennon",
+    instrument: "Guitar"
+  },
+  {
+    first_name: "Paul",
+    last_name: "McCartney",
+    instrument: "Bass Guitar"
+  },
+  # etc...
+]}
+
+File.open(filepath, 'wb') do |file|
+  string = JSON.generate(beatles)
+  file.write(string)
+end
